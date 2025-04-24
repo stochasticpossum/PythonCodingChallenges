074fda711e28d18e7bf6836f63cad949d3f89b8b
Adam Pintoro <adam.pintoro@gmail.com>
2025-04-24 19:36:32 -0400

    Thingd

--- challenge2/test.py
+++ challenge2/test.py
@@ -4,7 +4,7 @@ from challenge import test_https
 class TestChallenge(unittest.TestCase):
     def test_test_https(self):
         port_list = [80, 443, 8080]
-        target_host = "google.com"
+        target_host = "142.250.9.113"
         expected_result = [(80, True), (443, True), (8080, False)]
         self.assertEqual(test_https(port_list, target_host), expected_result)
 
