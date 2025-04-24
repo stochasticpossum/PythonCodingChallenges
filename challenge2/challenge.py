3bff16c6b63bca59e4cfaacbc22417b3a4a57d4d
adam.pintoro@relayhealth.com <adam.pintoro@relayhealth.com>
2025-04-24 19:33:55 -0400

    Iinit

---
+++ challenge2/challenge.py
@@ -0,0 +1,13 @@
+"""
+Challenge 2
+
+In this challenge you will write a function called test_https(). test_https will take two parameters:
+    1. port_list which is a list of TCP ports that the function will test
+    2. target_host which is a string that identifies the target host.
+
+The function should return a list of tuples. Each tuple should contain the port number and a boolean value indicating whether the port is open or closed.
+
+*** BONUS POINTS *** 
+
+SEnsure the function correctly validates IP addresses before attempting to connect to them.
+"""
---
+++ challenge2/test.py
@@ -0,0 +1,12 @@
+import unittest
+from challenge import test_https
+
+class TestChallenge(unittest.TestCase):
+    def test_test_https(self):
+        port_list = [80, 443, 8080]
+        target_host = "google.com"
+        expected_result = [(80, True), (443, True), (8080, False)]
+        self.assertEqual(test_https(port_list, target_host), expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
