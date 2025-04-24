e6aef926edfe6c963756001105bd79fd7258ec97
adam.pintoro@relayhealth.com <adam.pintoro@relayhealth.com>
2025-04-24 18:16:45 -0400

    2

--- challenge1/challenge.py
+++ challenge1/challenge.py
@@ -3,7 +3,7 @@
 """
 Challenge 1
 
-In this challenge you will write code to parse a CSV file that includes an employee database (stored at employee.csv). This database has five columns:
+In this challenge you will a fucntion called update_employee_database() to parse a CSV file that includes an employee database (stored at employee.csv). This database has five columns:
     * Employee Name (String)
     * Employee ID (alphanumeric String)
     * Employee Tenure (integerr indicating years of service)
